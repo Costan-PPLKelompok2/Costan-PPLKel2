5ebf42c1fa89204b25fafad2f685935bea5c1695
Dogedruga <haidirali4010@gmail.com>
2025-04-29 09:51:13 +0700

    Perbaikan manajemen data kos

--- dcost/app/Http/Controllers/KosController.php
+++ dcost/app/Http/Controllers/KosController.php
@@ -40,4 +40,54 @@ class KosController extends Controller
 
         return redirect()->route('kos.create')->with('success', 'Kos berhasil ditambahkan.');
     }
+
+    // Tampilkan form edit
+    public function edit($id)
+    {
+        $kos = \App\Models\Kos::where('user_id', auth()->id())->findOrFail($id);
+        return view('kos.edit', compact('kos'));
+    }
+
+    // Simpan hasil edit
+    public function update(Request $request, $id)
+    {
+        $kos = \App\Models\Kos::where('user_id', auth()->id())->findOrFail($id);
+
+        $request->validate([
+            'nama_kos' => 'required',
+            'deskripsi' => 'required',
+            'alamat' => 'required',
+            'harga' => 'required|numeric',
+            'fasilitas' => 'required',
+            'foto' => 'nullable|image|max:2048',
+        ]);
+
+        $data = $request->only(['nama_kos', 'deskripsi', 'alamat', 'harga', 'fasilitas']);
+
+        if ($request->hasFile('foto')) {
+            $data['foto'] = $request->file('foto')->store('foto_kos', 'public');
+        }
+
+        $kos->update($data);
+
+        return redirect()->route('kos.index')->with('success', 'Data kos berhasil diperbarui!');
+    }
+
+    // Hapus data kos
+    public function destroy($id)
+    {
+        $kos = \App\Models\Kos::where('user_id', auth()->id())->findOrFail($id);
+        $kos->delete();
+
+        return redirect()->route('kos.index')->with('success', 'Kos berhasil dihapus!');
+    }
+
+
+    // Menampilkan di dashboard
+    public function index()
+    {
+        $kosList = \App\Models\Kos::where('user_id', auth()->id())->get();
+        return view('kos.index', compact('kosList'));
+    }
+
 }
--- dcost/routes/web.php
+++ dcost/routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KosController;
+use Illuminate\Support\Facades\Auth;
+
 
 
 /*
@@ -16,6 +18,7 @@ use App\Http\Controllers\KosController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/',[HomeController::class,"index"]);
 
 Route::get('/dashboard', function () {
@@ -25,6 +28,10 @@ Route::get('/dashboard', function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('/kos/create', [KosController::class, 'create'])->name('kos.create');
     Route::post('/kos', [KosController::class, 'store'])->name('kos.store');
+    Route::get('/kos/{id}/edit', [KosController::class, 'edit'])->name('kos.edit');
+    Route::put('/kos/{id}', [KosController::class, 'update'])->name('kos.update');
+    Route::delete('/kos/{id}', [KosController::class, 'destroy'])->name('kos.destroy');
+    Route::get('/kos', [KosController::class, 'index'])->name('kos.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
